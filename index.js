c28fed7caca1137deeacb68e99b25de2301cd5d2
Anuja Suntnur <anujasuntnur@pop-os.localdomain>
2025-03-20 14:43:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+
+
+const app = express()  //variable app
+
+
+const port = 3000  //listen on this port
+
+app.get('/', (req, res) => {   //callback
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req, res)=>{
+    res.send('<h1>Helllllllo</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
